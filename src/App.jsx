b07318d66cc113a823c310f800ec64db76be13e2
pranjit-medhi <pranjit96medhi@gmail.com>
2024-05-26 10:23:57 +0530

    all files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Feature from './components/Feature'
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
+import Workflow from './components/Workflow'
+
+function App() {
+ 
+  return (
+    <>
+      <Navbar/>
+      <HeroSection/>
+      <Feature/>
+      <Workflow/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className='flex flex-col items-center mt-6 lg:mt-20'>
+        <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+            Virtual build tools
+            <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+                {" "} for developers
+            </span>
+        </h1>
+        <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality!
+        </p>
+        <div className="flex justify-center my-10">
+            <a href='#' className='bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md'>
+                Start for free
+            </a>
+            <a href="#" className="py-3 px-4 mx-3 rounded-md">Documentation</a> 
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
